b1c13f5680d0827974c446efac70276d8d3c9851
 Dmitry <dmitry@Dmitrys-MacBook-Pro.local>
2017-07-10 15:26:12 +0300

    Updated readme, package and app files

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Test environment for building scalable applications, based on technology:
 - NodeJS
 - ExpressJS
 - CouchDB
+- ejs - templating language
+
+# Install project
+- npm install
+- npm start
--- app.js
+++ app.js
@@ -14,6 +14,13 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use('/', router)
 
 app.listen(PORT, HOSTNAME, (err) => {
